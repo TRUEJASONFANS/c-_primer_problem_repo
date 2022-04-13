ed3fb95172c3a1ef33a058660d41a5eaa9c11045
zhxie <Zhong-wei.xie@asml.com>
2022-04-13 14:05:59 +0800

    add the test for sizeof of different align class member

---
+++ memory_align/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+typedef std::pair<int, double> Dir;
+
+class B {
+
+};
+
+class A {
+private:
+    Dir d; //16
+    int i; // 4
+    bool b1; // 4(1)
+
+};
+
+class C {
+    double d1;
+    Dir d;
+};
+
+
+using namespace  std;
+int main() {
+    Dir d;
+    cout << "Dir sizeof:" << sizeof(d) << endl;
+    A a;
+    cout << "a sizeof:" << sizeof(a) << endl;
+    cout << "B*: sizeof" << sizeof(B*) << endl;
+    cout << "C: sizeof" << sizeof(C) << endl;
+
+    string s;
+    cout << "string sizeof: " << sizeof(s);
+    s = "123456789";
+    cout << "string sizeof: " << sizeof(s);
+    return 0;
+}
