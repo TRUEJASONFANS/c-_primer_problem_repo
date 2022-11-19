d420cfed98ea11fd51577a696af979841aa89b08
truejasonfans <472408570@qq.com>
2022-11-19 14:18:01 +0800

    A move constructor example

--- template/move/TestTemplateConstructor.h
+++ template/move/TestTemplateConstructor.h
@@ -5,14 +5,31 @@
 #ifndef C___PRIMER_PROBLEMS_TESTTEMPLATECONTRUTOR_H
 #define C___PRIMER_PROBLEMS_TESTTEMPLATECONTRUTOR_H
 
+using std::cout;
+using std::endl;
+
 class B {
+public:
+    B(int a) {
+        this->a = a;
+        cout << "constructor" << endl;
+    }
+
+    B(B&& b2) {
+        a = std::move(b2.a);
+        cout << "move constructor" << endl;
+    }
+private:
+    int a;
 
 };
 class A {
 public:
     template<class T>
     A (T&& t):b_(std::move(t))
-    {}
+    {
+
+    }
 
 private:
     B b_;
