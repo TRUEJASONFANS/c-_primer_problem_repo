ef78952b639c40ee5a94e9ace79b787290c78688
truejasonfans <472408570@qq.com>
2022-12-22 10:08:08 +0800

    unique_ptr usage

--- memory/main.cpp
+++ memory/main.cpp
@@ -4,12 +4,27 @@
 
 #include <iostream>
 #include <memory>
-#include <cassert>
+#include "UniquePtrTest.h"
 
 using  std::cout;
 using std::string;
 int main() {
     auto nullPtr = std::unique_ptr<string>(nullptr);
-    assert(nullPtr);
+
+    // 创建初始ptr
+    std::unique_ptr<int> ptr(new int(10));
+
+    // 1 传引用
+    std::cout << asRef(ptr) << std::endl;
+
+    // 2 移交所有权
+    /*
+        作为参数传入时release
+        一定要取得返回值，否则内存泄漏
+    */
+    int re =  asOwner(std::move(ptr));
+
+    std::cout << re << std::endl;
+
     return 0;
 }
