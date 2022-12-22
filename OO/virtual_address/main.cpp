8d8036cbd0000d93f8ba717c51012e3fccecb1be
truejasonfans <472408570@qq.com>
2022-12-22 10:08:44 +0800

    虚函数实现原理

--- OO/virtual_address/main.cpp
+++ OO/virtual_address/main.cpp
@@ -11,6 +11,7 @@ public:
     virtual void fun1() { cout << "Base::fun1" << endl; }
     virtual void fun2() { cout << "Base::fun2" << endl; }
     virtual void fun3() { cout << "Base::fun3" << endl; }
+    const std::string s = "x";
 };
 
 class A : public Base
@@ -23,42 +24,38 @@ public:
 typedef void(*Fun)();
 
 int main() {
-
+    typedef void (*F)();
     Base* base = new Base();
-    long* p1 = (long*)base; // 虚表 指针， *p1 是虚表地址
-    long* p2 = (long*)(*p1);
+    cout << "base pointer:" << base<< endl;
+    intptr_t* p1 = (intptr_t*)base; // 虚表 指针， *p1 是虚表地址
+    cout << "virtual pointer address:" << *p1 << endl;
+    intptr_t p2 = *(intptr_t*)(*p1);
+    cout << p2 << endl;
     for (int i = 0; i < 3; ++i) {
-        cout << p2[i] << ' ';
+        void* funcAddr = (intptr_t*)p2 + sizeof(intptr_t) * i;
+        cout << funcAddr << ' ';
+        ((F)(funcAddr))();
     }
     cout << endl;// p2[0], p2[1], p2[2]
 
-    A* a = new A();
-    long* p3 = (long*)a;
-    long* p4 = (long*)(*p3);
-    for (int i = 0; i < 3; ++i) {
-        cout << p4[i] << ' ';
-    }
-    cout << endl; // p4[0] == p4[0] , p4[1], p2[2] == p4[2]
+//    A* a = new A();
+//    long* p3 = (long*)a;
+//    long* p4 = (long*)(*p3);
+//    for (int i = 0; i < 3; ++i) {
+//        cout << p4[i] << ' ';
+//    }
+//    cout << endl; // p4[0] == p4[0] , p4[1], p2[2] == p4[2]
 
 
-    typedef void (*F)();
-
-    F f1 = (F)p2[0];
-    F f2 = (F)p2[1];
-    F f3 = (F)p2[2];
-    f1(); //Base::fun1
-    f2(); //Base::fun2
-    f3(); //Base::fun3
-
-    a = new A();
-    long* p5 = (long*)a;
-    long* p6 = (long*)(*p3);
-    F f4 = (F)p6[0];
-    F f5 = (F)p6[1];
-    F f6 = (F)p6[2];
-
-    f4(); //Base::fun1
-    f5(); //A::fun2`
-    f6(); //Base::fun3
+//    a = new A();
+//    long* p5 = (long*)a;
+//    long* p6 = (long*)(*p3);
+//    F f4 = (F)p6[0];
+//    F f5 = (F)p6[1];
+//    F f6 = (F)p6[2];
+//
+//    f4(); //Base::fun1
+//    f5(); //A::fun2`
+//    f6(); //Base::fun3
     return 0;
 }
