8a9c0be9342c0ab7411d587bd542984df9386981
truejasonfans <472408570@qq.com>
2023-10-09 00:44:27 +0800

    add tuple template case

--- template/container/test/main.cpp
+++ template/container/test/main.cpp
@@ -11,14 +11,14 @@ struct LIST2{};
 
 
 int main(){
-    type_list<int, float, bool> l;
-    static_assert(std::is_same_v<typename front<decltype(l)>::type, int>);
+    container::type_list<int, float, bool> l;
+    static_assert(std::is_same_v<typename container::front<decltype(l)>::type, int>);
 
     std::tuple<int, float, bool> t;
-    front_t<decltype(t)> first;
+    container::front_t<decltype(t)> first;
 
     LIST2<int, bool, bool> a;
-    front_t<decltype(a)> first2;
+    container::front_t<decltype(a)> first2;
 
-    bool b = contains_type_v<bool, decltype(t)>;
+    bool b = container::contains_type_v<bool, decltype(t)>;
 }
